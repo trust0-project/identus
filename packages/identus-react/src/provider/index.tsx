4f0e7be790f57d58e6788f04401f69112e86727e
Javier Ribó <elribonazo@gmail.com>
2025-07-07 00:51:46 +0200

    fix: export providers Signed-off-by: Javier Ribó <elribonazo@gmail.com>

--- packages/identus-react/src/provider/index.tsx
+++ packages/identus-react/src/provider/index.tsx
@@ -6,3 +6,11 @@
  */ 
 
 export * from "./Agent";
+export* from './Connections';
+export * from './Credentials';
+export * from './Database';
+export * from './DID';
+export * from './HolderProvider';
+export * from './Messages';
+export * from './Issuer';
+export * from './VerifierProvider';
