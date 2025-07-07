b99ab6440fc9dd1c0ae741a7c775024129ecd2eb
Javier Ribó <elribonazo@gmail.com>
2025-07-07 14:22:32 +0200

    fix: credentials fetching Signed-off-by: Javier Ribó <elribonazo@gmail.com>

--- packages/identus-react/src/provider/Credentials.tsx
+++ packages/identus-react/src/provider/Credentials.tsx
@@ -1,56 +1,28 @@
 import React, { useCallback, useEffect, useState } from "react";
 import SDK from "@hyperledger/identus-sdk";
 
-import { useRIDB } from "@trust0/ridb-react";
 import { CredentialsContext } from "../context";
-import { useAgent, useMessages } from "../hooks";
-import { schemas } from "../db";
+import { useAgent, useDatabase } from "../hooks";
 import { hasDB } from "../utils";
 
-type ExtendedMessage = { message: SDK.Domain.Message; read: boolean; }
 
 export function CredentialsProvider({ children }: { children: React.ReactNode }) {
     const { agent } = useAgent(); 
-    const { messages: messagesListener } = useMessages();
-    const { db, state: dbState } = useRIDB<typeof schemas>();
+    const { db, state: dbState, pluto } = useDatabase();
     const [credentials, setCredentials] = useState<SDK.Domain.Credential[]>([]);
 
-    const filterMessages = useCallback((messages: ExtendedMessage[]) => new Set(
-        messages
-        .filter(({message}) => message.piuri === SDK.ProtocolType.DidcommIssueCredential)
-        .map(({message}) => message.id)
-    ), []);
-
     const fetchCredentials = useCallback(async () => {
-        if (agent && dbState === "loaded") {
-            const credentials = await agent?.pluto.getAllCredentials();
+        if (dbState === "loaded") {
+            const credentials = await pluto.getAllCredentials();
             setCredentials(prev => [...prev, ...credentials.filter((credential) => !prev.some((c) => c.uuid === credential.uuid))]);
             return credentials ?? [];
         }
         return []
-    }, [agent, setCredentials, dbState]);
-
-    const [
-        issueCredentialMessages, 
-        setIssueCredentialMessages
-    ] = useState<Set<string>>(filterMessages(messagesListener))
+    }, [pluto, setCredentials, dbState]);
 
     useEffect(() => {
         fetchCredentials()
     }, [fetchCredentials, setCredentials]);
-
-    useEffect(() => {
-        const newMessages = filterMessages(messagesListener);
-        if (agent && newMessages.size > issueCredentialMessages.size) {
-            if (issueCredentialMessages.size > 0) {
-                const credentialMessages = messagesListener
-                .filter(({message}) => message.piuri === SDK.ProtocolType.DidcommIssueCredential)
-                .map(({message}) => message);
-                Promise.all(credentialMessages.map((message) => agent.handle(message)))
-            }
-            setIssueCredentialMessages(newMessages);
-        }
-    }, [fetchCredentials, messagesListener, issueCredentialMessages]);
 
     const deleteCredential = useCallback(async (credential: SDK.Domain.Credential) => {
         if (!hasDB(db) || dbState !== "loaded") {
