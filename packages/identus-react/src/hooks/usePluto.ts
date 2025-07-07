e70d2004708c48cb2000ff7ceeb93e9283a9a668
Javier Ribó <elribonazo@gmail.com>
2025-07-07 04:42:06 +0200

    fix: change usePluto Signed-off-by: Javier Ribó <elribonazo@gmail.com>

--- packages/identus-react/src/hooks/usePluto.ts
+++ packages/identus-react/src/hooks/usePluto.ts
@@ -2,10 +2,10 @@ import SDK from "@hyperledger/identus-sdk";
 import { createStore } from "@trust0/identus-store";
 import { StorageType } from "@trust0/ridb";
 import { BaseStorage } from "@trust0/ridb-core";
-import { useRIDB } from "@trust0/ridb-react";
 import { useMemo } from "react";
 import { schemas } from "../db";
 import { useApollo } from "./useApollo";
+import { useDatabase } from "./useDatabase";
 
 /**
  * Creates and returns a memoized Pluto instance for persistent storage operations.
@@ -18,7 +18,7 @@ import { useApollo } from "./useApollo";
  *
  */
 export function usePluto(storageType: typeof BaseStorage | StorageType = StorageType.IndexDB) {
-    const {db} = useRIDB<typeof schemas>()
+    const {db} = useDatabase()
     const store = useMemo(() => createStore({ db, storageType }), [db])
     const apollo = useApollo()
     const pluto = useMemo(() => new SDK.Pluto(store, apollo), [store, apollo])
